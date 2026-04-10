a7b4a9f9d2a141e88ba5ddb61554c001a8f1af8c
JassuPv <shubhamjaiswal@JassuPvs-MacBook-Air.local>
2026-04-10 12:43:33 +0530

    Two sum for unsorted arrays

---
+++ two-pointers/two-sum-unsorted.js
@@ -0,0 +1,16 @@
+function twoSum(nums, target) {
+    const pairIdx = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        if (target - num in pairIdx) {
+            return [i, pairIdx[target - num]];
+        }
+        pairIdx[num] = i;
+    }
+}
+
+// Example usage:
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target)); // Output: [0, 1] || [1, 0]
